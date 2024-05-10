24e30fa287b7fc864861891167ad33c3a7ca0bd2
ShaharAshe <shaharas30@gmail.com>
2024-05-10 16:23:42 +0300

    Fixed the useEffect//

--- src/components/CardRendering.js
+++ src/components/CardRendering.js
@@ -7,24 +7,26 @@ function CardRendering(props){
     const initializeImages = () => {
         const newImages = {};
         for (let i = 0; i <= 15; ++i) {
-            newImages[i] = '\\images\\'+ i + '.jpg'; // Example path
+            newImages[i] = '\\images\\' + i + '.jpg'; // Example path
         }
-        for (let i = 0; i < 15 ; ++i) {
-            if(i%2!==0){
+        for (let i = 0; i < 15; ++i) {
+            if (i % 2 !== 0) {
                 let temp = newImages[0];
-                newImages[0] = newImages[15-1-i];
-                newImages[15-1-i] = temp;
+                newImages[0] = newImages[15 - 1 - i];
+                newImages[15 - 1 - i] = temp;
             }
         }
         setImages(newImages);
+    };
+
+    const createCards = (images) => {
         const upCards = [];
         for (let i = 0; i < props.inputs.rows; i++) {
             const row = []
             for (let j = 0; j < props.inputs.cols; j++) {
-                {console.log(i + j + (props.inputs.cols-1))}
                 row.push(
                     <Col xs={12/props.inputs.cols} key={i + j + (props.inputs.cols-1)} className={((i + j) * (i + 1)).toString()}>
-                        <img src={images[i + j + (props.inputs.cols-1)]} alt="img"/>
+                        <img src={images[i + j + (props.inputs.cols-1)]} className="img-fluid" alt="img"/>
                     </Col>
                 );
             }
@@ -32,18 +34,18 @@ function CardRendering(props){
         }
         props.updateCards(upCards);
     };
+
     useEffect(() => {
         initializeImages();
     },[]);
+    useEffect(() => {
+        if (Object.keys(images).length > 0) {
+            createCards(images); // Create cards when images are set
+        }
+    }, [createCards, images]);
 
-
-
-    const cards = props.cards;
-// <Col key={key} className={key}>
-    //     <img src={value} alt="img"/>
-    // </Col>
     return (
-        <div>{cards}</div>
+        <div>{props.cards}</div>
     );
 }
 export default CardRendering;
